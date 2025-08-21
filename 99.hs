2ab1b45574c6242c0ab3dbd21d5b6edcc82c6a2e
Pedro Oliveira <pedro.freire.oliveira@tecnico.ulisboa.pt>
2025-08-21 20:27:33 +0100

    Initial commit.

---
+++ 99.hs
@@ -0,0 +1,41 @@
+-- 00: Simple Naive Fibbonacci in Haskell
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1 
+fib n = fib (n-1) + fib (n-2)
+
+-- 01: Find the last element of a list!
+myLast :: [a] -> a
+myLast [] = error "There's no last element in an empty list!"
+myLast [x] = x
+myLast (_:x) = myLast x
+
+-- 02: Find the second last element of a list!
+mySecondLast :: [a] -> a
+mySecondLast [] = error "There's no second last element in an empty list!"
+mySecondLast [b] = error "There's no second last element in a one-element list"
+mySecondLast [x, y] = x
+mySecondLast (_:xs) = mySecondLast xs
+
+-- 03: Find the kth-1 element of a list!
+elementAt :: [a] -> Int -> a
+elementAt [] n = error "There's no elements in an empty list"
+elementAt (x:_) 1 = x
+elementAt (_:xs) n = elementAt xs (n-1)
+
+-- 04: Find the length of a list
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs 
+
+-- 05: Reverse a list
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse [x] = [x]
+myReverse (h:xs) = (myReverse xs) ++ [h]
+
+-- 06: Is Palindrome?
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome lst = (reverse lst) == lst
+
+ 

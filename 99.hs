be6b911a79648c0a64e91187f3e34cf58bd6f7eb
Pedro Oliveira <pedro.freire.oliveira@tecnico.ulisboa.pt>
2026-03-01 15:34:07 +0000

    Completed 09, 10, 11 and 12.

--- 99.hs
+++ 99.hs
@@ -5,22 +5,22 @@ fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
 -- 01: Find the last element of a list!
-myLast :: [a] -> a
-myLast [] = error "There's no last element in an empty list!"
-myLast [x] = x
+myLast :: [a] -> Maybe a
+myLast [] = Nothing
+myLast [x] = Just x
 myLast (_:x) = myLast x
 
 -- 02: Find the second last element of a list!
-mySecondLast :: [a] -> a
-mySecondLast [] = error "There's no second last element in an empty list!"
-mySecondLast [b] = error "There's no second last element in a one-element list"
-mySecondLast [x, y] = x
+mySecondLast :: [a] -> Maybe a
+mySecondLast [] = Nothing
+mySecondLast [b] = Nothing
+mySecondLast [x, y] = Just x
 mySecondLast (_:xs) = mySecondLast xs
 
 -- 03: Find the kth-1 element of a list!
-elementAt :: [a] -> Int -> a
-elementAt [] n = error "There's no elements in an empty list"
-elementAt (x:_) 1 = x
+elementAt :: [a] -> Int -> Maybe a
+elementAt [] n = Nothing
+elementAt (x:_) 1 = Just x
 elementAt (_:xs) n = elementAt xs (n-1)
 
 -- 04: Find the length of a list
@@ -50,6 +50,30 @@ myCompress [] = []
 myCompress [a] = [a]
 myCompress (a:b:xs) = if (a == b) then myCompress(b : xs) else a : myCompress(b : xs)
 
--- 09: Pack a List.
+-- 09: Pack duplicates in a list.
+myPack :: Eq a => [a] -> [[a]]
+myPack [] = []
+myPack (x:xs) = (takeWhile (==x) (x:xs)) : myPack (dropWhile (==x) (x:xs))
 
+-- 10: Run-length encoding of a list.
+myEncode :: Eq a => [a] -> [(Int, a)]
+myEncode [] = []
+myEncode lst = map (\pack -> (length pack, pack !! 0)) (myPack lst)
 
+-- 11: Modified run-length encoding.
+data L11 a = Single a | Multiple Int a
+    deriving (Show)
+
+myEncodeModified :: Eq a => [a] -> [L11 a]
+myEncodeModified lst = map translate (myEncode lst)
+  where translate = (\(len, elm) ->
+                       if len == 1 then Single elm
+                       else Multiple len elm)
+
+-- 12: Decode modified run-length encoding.
+myDecodeModified :: Eq a => [L11 a] -> [a]
+myDecodeModified lst = concat $ map decode lst
+  where decode = (\l ->
+                    case l of
+                      Single elm     -> [elm]
+                      Multiple n elm -> replicate n elm)
